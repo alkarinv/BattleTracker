2162a6630e6f4c185dc837a0ad09882173ac3eca
alkarin <alkarin.v@gmail.com>
2012-09-04 12:12:50 -0500

    Fix for VersusRecords when names had > 32 chars

--- src/mc/alk/tracker/objects/IdTooLongException.java
+++
@@ -1,9 +0,0 @@
-package mc.alk.tracker.objects;
-
-public class IdTooLongException extends Exception {
-	public IdTooLongException(String string) {
-		super(string);
-	}
-
-	private static final long serialVersionUID = 1L;
-}
